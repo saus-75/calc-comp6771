be0a7b6759a4700d01c38f939a2f3d3bf1add9f7
saus-75 <georgechieng@gmail.com>
2017-08-08 18:06:30 +1000

    started, using three stacks to differentiate between double and int

--- calculator.cpp
+++ calculator.cpp
@@ -2,6 +2,9 @@
 #include <fstream>
 #include <iomanip>
 #include <cmath>
+#include <queue>
+#include <stack>
+#include <string>
 
 int main(int argc, char* argv[]) {
 
@@ -15,27 +18,58 @@ int main(int argc, char* argv[]) {
 
     std::cout << argv[1] << std::endl;
 
-    // string to be read into
+    // Initation
     std::string s;
-    // std::stack<double> number;
+    std::queue <std::string> op;
+    std::stack <int> intStack;
+    std::stack <double> doubleStack;
+    std::stack <std::string> typeStack;
 
-    // read the file while we have input.
-    double x = 234.090;
-    if (x == std::floor(x)){
-        std::cout << "int" << "\n";
-        int v = x;
-        std::cout << v << "\n";
-    } else {
-        std::cout << x << "\n";
-    }
+
+    // read the file while we have input and shove it into a queue.
     while (in >> s) {
-        if (s.find('.') != std::string::npos){
-            double d = std::stod(s);
-            std::cout << d << "\n";
-            // std::number.push(s);
-        } else {
-            std::cout << s << "\n";
-        }
+        op.push(s);
     }
+
+    // loop from queue
+    while (!op.empty()){
+        std::string frontElem = op.front();
+        if (frontElem.find('.') != std::string::npos){
+            double d = std::stod(frontElem);
+            doubleStack.push(d);
+            typeStack.push("double");
+            std::cout << "double stack: " << doubleStack.top() << "\n";
+        } else if (std::isdigit(frontElem[0])){
+            int i = std::stoi(frontElem);
+            intStack.push(i);
+            typeStack.push("integer");
+            std::cout << "integer stack: " << intStack.top() << "\n";
+        } else {
+            if (frontElem.find('add')){
+                auto var1;
+                auto var2;
+
+                var1 = popper(typeStack, doubleStack, intStack);
+                var2 = popper(typeStack, doubleStack, intStack);
+            } else if (frontElem.find('sub')){
+                std::cout << "operator: " << op.front() << "\n";
+            } else if (frontElem.find('div')){
+                std::cout << "operator: " << op.front() << "\n";
+            } else if (frontElem.find('sqrt')){
+                std::cout << "operator: " << op.front() << "\n";
+            } else if (frontElem.find('pop')){
+                std::cout << "operator: " << op.front() << "\n";
+            } else if (frontElem.find('reverse')){
+                std::cout << "operator: " << op.front() << "\n";
+            } else if (frontElem.find('repeat')){
+                std::cout << "operator: " << op.front() << "\n";
+            } else if (frontElem.find('endrepeat')){
+                std::cout << "operator: " << op.front() << "\n";
+            }
+        }
+        op.pop();
+    }
+
+    //close input file
     in.close();
 }
