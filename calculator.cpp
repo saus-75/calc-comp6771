2d84d37224603eacbb9eb8971a008ce46c6963ce
saus-75 <georgechieng@gmail.com>
2017-08-11 17:39:00 +1000

    majority of the stuff done, need to functionalise everything

--- calculator.cpp
+++ calculator.cpp
@@ -6,8 +6,7 @@
 #include <string>
 #include <queue>
 
-void autoStacker(std::stack<std::string> &ts, std::stack<double> &ds, std::stack<int> &is, std::string var);
-auto autoPoppper(std::stack<std::string> &ts, std::stack<double> &ds, std::stack<int> &is);
+void ptr(std::stack<std::string> stk);
 
 int main(int argc, char* argv[]) {
 
@@ -19,85 +18,162 @@ int main(int argc, char* argv[]) {
     std::ifstream in;
     in.open(argv[1]);
 
-    std::cout << argv[1] << std::endl;
+    //seg fault will occur if theres no input
+    if (argv[1] != NULL){
+        std::cout << argv[1] << std::endl;
+    } else {
+        std::cout << "Please input a valid filename" << std::endl;
+    }
 
     // Initation
     std::string s;
     std::queue <std::string> op;
-    std::stack <double> doubleStack;
-    std::stack <int> intStack;
-    std::stack <std::string> typeStack;
+    std::stack <std::string> numbers;
+    std::stack <std::string> stkPtr;
 
-
+ 
     // read the file while we have input and shove it into a queue.
     while (in >> s) {
         op.push(s);
+        stkPtr.push(s);
     }
 
     // loop from queue
     while (!op.empty()){
         std::string frontElem = op.front();
-        if (frontElem.find('.') != std::string::npos){
-            autoStacker(typeStack, doubleStack, intStack, frontElem);
-        } else if (std::isdigit(frontElem[0])){
-            autoStacker(typeStack, doubleStack, intStack, frontElem);
+        if (std::isdigit(frontElem[0]) || std::isdigit(frontElem[1])){
+            numbers.push(frontElem);
+            // std::cout << "number: " << numbers.top() << "\n";
         } else {
-            if (frontElem.find("add")){
-                auto var1 = autoPoppper(typeStack, doubleStack, intStack);
-                auto var2 = autoPoppper(typeStack, doubleStack, intStack);
+            if (frontElem.find("add") != std::string::npos){
+                auto var1 = numbers.top();
+                numbers.pop();
+                auto var2 = numbers.top();
+                numbers.pop();
+                if ((var1.find('.') != std::string::npos) || (var2.find('.') != std::string::npos)){
+                    double x = std::stod(var1);
+                    double y = std::stod(var2);
+                    double z = x + y;
+                    numbers.push(std::to_string(z));
+                    std::cout << x << " + " << y << " = " << z << "\n";
+                } else {
+                    int x = std::stoi(var1);
+                    int y = std::stoi(var2);
+                    int z = x + y;
+                    numbers.push(std::to_string(z));
+                    std::cout << x << " + " << y << " = " << z << "\n";
+                }
+            } else if (frontElem.find("sub") != std::string::npos){
+                auto var1 = numbers.top();
+                numbers.pop();
+                auto var2 = numbers.top();
+                numbers.pop();
+                if ((var1.find('.') != std::string::npos) || (var2.find('.') != std::string::npos)){
+                    double x = std::stod(var1);
+                    double y = std::stod(var2);
+                    double z = x - y;
+                    numbers.push(std::to_string(z));
+                    std::cout << x << " - " << y << " = " << z << "\n";
+                } else {
+                    int x = std::stoi(var1);
+                    int y = std::stoi(var2);
+                    int z = x - y;
+                    numbers.push(std::to_string(z));
+                    std::cout << x << " - " << y << " = " << z << "\n";
+                }
+            } else if (frontElem.find("div") != std::string::npos){
+                auto var1 = numbers.top();
+                numbers.pop();
+                auto var2 = numbers.top();
+                numbers.pop();
+                if ((var1.find('.') != std::string::npos) || (var2.find('.') != std::string::npos)){
+                    double x = std::stod(var1);
+                    double y = std::stod(var2);
+                    double z = x / y;
+                    numbers.push(std::to_string(z));
+                    std::cout << x << " / " << y << " = " << z << "\n";
+                } else {
+                    int x = std::stoi(var1);
+                    int y = std::stoi(var2);
+                    int z = x / y;
+                    numbers.push(std::to_string(z));
+                    std::cout << x << " / " << y << " = " << z << "\n";
+                }
+            } else if (frontElem.find("mult") != std::string::npos){
+                auto var1 = numbers.top();
+                numbers.pop();
+                auto var2 = numbers.top();
+                numbers.pop();
+                if ((var1.find('.') != std::string::npos) || (var2.find('.') != std::string::npos)){
+                    double x = std::stod(var1);
+                    double y = std::stod(var2);
+                    double z = x * y;
+                    numbers.push(std::to_string(z));
+                    std::cout << x << " * " << y << " = " << z << "\n";
+                } else {
+                    int x = std::stoi(var1);
+                    int y = std::stoi(var2);
+                    int z = x * y;
+                    numbers.push(std::to_string(z));
+                    std::cout << x << " * " << y << " = " << z << "\n";
+                }
+            } else if (frontElem.find("sqrt") != std::string::npos){
+                auto var = numbers.top();
+                numbers.pop();
 
-                auto sum = var1 + var2;
-                autoStacker(typeStack, doubleStack, intStack, sum);
-                std::cout << var1 << " + " << var2 << " = " << sum << "\n";
-            } else if (frontElem.find("sub")){
-                std::cout << "operator: " << op.front() << "\n";
-            } else if (frontElem.find("div")){
-                std::cout << "operator: " << op.front() << "\n";
-            } else if (frontElem.find("sqrt")){
-                std::cout << "operator: " << op.front() << "\n";
-            } else if (frontElem.find("pop")){
-                std::cout << "operator: " << op.front() << "\n";
-            } else if (frontElem.find("reverse")){
-                std::cout << "operator: " << op.front() << "\n";
-            } else if (frontElem.find("repeat")){
-                std::cout << "operator: " << op.front() << "\n";
-            } else if (frontElem.find("endrepeat")){
-                std::cout << "operator: " << op.front() << "\n";
+                if (var.find('.') != std::string::npos){
+                    double x = std::stod(var);
+                    double y = std::sqrt(x);
+                    numbers.push(std::to_string(y));
+                    std::cout << "sqrt " << x << " = " << y << "\n";
+                } else {
+                    int x = std::stoi(var);
+                    int y = std::sqrt(x);
+                    numbers.push(std::to_string(y));
+                    std::cout << "sqrt " << x << " = " << y << "\n";
+                }
+            } else if (frontElem.find("pop") != std::string::npos){
+                std::cout << numbers.top() << " Popped\n";
+                numbers.pop();
+            } else if (frontElem.find("reverse") != std::string::npos){
+                std::queue <std::string> reverse;
+                int nth = std::stoi(numbers.top());
+                numbers.pop();
+                std::cout << "blocks of number to reverse: " << nth << "\n";
+                for (int i = 0; i < nth; ++i){
+                    reverse.push(numbers.top());
+                    numbers.pop(); 
+                }
+                for (int j = 0; j < nth; ++j)
+                {
+                    numbers.push(reverse.front());
+                    reverse.pop();
+                }
+            } else if (frontElem.find("endrepeat") != std::string::npos){
+                std::cout << "endrepeat operator: " << frontElem << "\n";
+            } else if (frontElem.find("repeat") != std::string::npos){
+                int nth = std::stoi(numbers.top());
+                numbers.pop();
+                std::cout << "Number of repeats: " << nth << "\n";
             }
         }
+        // std::cout << "popping: " <<op.front() << "\n";
         op.pop();
     }
-
+    ptr(numbers);
     //close input file
     in.close();
 }
 
-void autoStacker(std::stack<std::string> &ts, std::stack<double> &ds, std::stack<int> &is, std::string var){
-    if (var.find('.') != std::string::npos){
-        double d = std::stod(var);
-        ds.push(d);
-        ts.push("double");
-        //debugger
-        std::cout << "PUSHED: double stack: " << ds.top() << "\n";
-    } else if (std::isdigit(var[0])){
-        int i = std::stoi(var);
-        is.push(i);
-        ts.push("integer");
-        //debugger
-        std::cout << "PUSHED: integer stack: " << is.top() << "\n";
-    }
+void checker(std::stack<std::string> &stk, string x, string y){
+    
 }
 
-auto autoPoppper(std::stack<std::string> &ts, std::stack<double> &ds, std::stack<int> &is){
-    auto var;
-    string typeCheck = ts.top();
-    if (typeCheck.find("double")){
-        var = ds.top();
-        ds.pop();
-    } else {
-        var = is.pop();
-        is.pop();
+void ptr(std::stack<std::string> stk){
+    std::cout << "--------------------\n";
+    while (!stk.empty()){
+        std::cout << "\t" << stk.top() << "\n";
+        std::cout << "--------------------\n";
+        stk.pop();
     }
-    ts.pop();
-    return var;
 }
